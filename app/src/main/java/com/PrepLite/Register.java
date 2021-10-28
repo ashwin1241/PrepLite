a24eb3d4a7e3c58e4c446a92e1fbd02490586e6b
ashwin1241 <ashwinwadatkar@gmail.com>
2021-10-28 23:42:33 +0530

    Splashscreen done

---
+++ app/src/main/java/com/PrepLite/Register.java
@@ -0,0 +1,29 @@
+package com.PrepLite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Register extends AppCompatActivity {
+
+    private TextView lgn_link;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        lgn_link = findViewById(R.id.login_link);
+        lgn_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this,Login.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
