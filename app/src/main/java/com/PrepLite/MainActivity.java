2f60943b72d0ca8706ad42e25f75e97c697d96ef
ashwin1241 <ashwinwadatkar@gmail.com>
2021-10-29 17:21:32 +0530

    Bug fixes

--- app/src/main/java/com/PrepLite/MainActivity.java
+++ app/src/main/java/com/PrepLite/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this,Register.class);
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
                 startActivity(intent);
             }
         },3000);
