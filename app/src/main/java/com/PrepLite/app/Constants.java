574de6d378f0b79021306b150c86c0b86c6a70f6
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2021-11-14 12:16:07 +0530

    Created a package for app level info

---
+++ app/src/main/java/com/PrepLite/app/Constants.java
@@ -0,0 +1,9 @@
+package com.PrepLite.app;
+
+public class Constants {
+
+    public static final String BASE_URL = "http://82a7-157-48-248-163.ngrok.io/";
+
+    public static final String REGISTER_USER = "register/";
+
+}
