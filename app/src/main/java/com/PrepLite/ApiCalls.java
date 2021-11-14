948b5b7f3cbe377fce93fff5a3bf04eb7bfb1079
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2021-11-14 12:13:22 +0530

    Updated the content-type to json instead of url encoded

--- app/src/main/java/com/PrepLite/ApiCalls.java
+++ app/src/main/java/com/PrepLite/ApiCalls.java
@@ -1,25 +1,33 @@
 package com.PrepLite;
 
+import static com.PrepLite.app.Constants.REGISTER_USER;
+
 import com.PrepLite.response.ResponseLogin;
 import com.PrepLite.response.ResponseRegister;
 
+import java.util.HashMap;
+
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface ApiCalls {
 
-    @FormUrlEncoded
-    @POST("register")
-    Call<ResponseRegister> register(
-            @Field("name") String name,
-            @Field("email") String email,
-            @Field("password") String password,
-            @Field("yearOfStudy") String year,
-            @Field("alumni") String alumni
+//    @FormUrlEncoded
+//    @POST("register")
+//    Call<ResponseRegister> register(
+//            @Field("name") String name,
+//            @Field("email") String email,
+//            @Field("password") String password,
+//            @Field("yearOfStudy") String year,
+//            @Field("alumni") String alumni
+//
+//    );
 
-    );
+    @POST(REGISTER_USER)
+    Call<ResponseRegister> register(@Body HashMap<String, String> registerCredentials);
 
     @FormUrlEncoded
     @POST("login")
