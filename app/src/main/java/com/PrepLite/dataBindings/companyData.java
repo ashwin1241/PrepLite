b8d091a06031e94b6c82ca05eff66b5380eabc5a
ashwin1241 <ashwinwadatkar@gmail.com>
2021-11-24 12:38:34 +0530

    insti replicated from company

--- app/src/main/java/com/PrepLite/dataBindings/companyData.java
+++ app/src/main/java/com/PrepLite/dataBindings/companyData.java
@@ -4,10 +4,12 @@ public class companyData {
 
     private String companyName;
     private String companyImage;
+    private long no_participants;
 
-    public companyData(String companyName, String companyImage) {
+    public companyData(String companyName, String companyImage, long no_participants) {
         this.companyName = companyName;
         this.companyImage = companyImage;
+        this.no_participants = no_participants;
     }
 
     public String getCompanyName() {
@@ -24,5 +26,13 @@ public class companyData {
 
     public void setCompanyImage(String companyImage) {
         this.companyImage = companyImage;
+    }
+
+    public long getNo_participants() {
+        return no_participants;
+    }
+
+    public void setNo_participants(long no_participants) {
+        this.no_participants = no_participants;
     }
 }
