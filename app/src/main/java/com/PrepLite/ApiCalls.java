d5daa68f19cf70ff44761c519f3aab3422795a09
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2021-11-26 16:10:22 +0530

    Integrated login and profile endpoints

--- app/src/main/java/com/PrepLite/ApiCalls.java
+++ app/src/main/java/com/PrepLite/ApiCalls.java
@@ -1,39 +1,26 @@
 package com.PrepLite;
 
+import static com.PrepLite.app.Constants.LOGIN_USER;
+import static com.PrepLite.app.Constants.PROFILE_USER;
 import static com.PrepLite.app.Constants.REGISTER_USER;
 
-import com.PrepLite.response.ResponseLogin;
-import com.PrepLite.response.ResponseRegister;
+import com.PrepLite.response.ServerResponse;
 
 import java.util.HashMap;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface ApiCalls {
 
-//    @FormUrlEncoded
-//    @POST("register")
-//    Call<ResponseRegister> register(
-//            @Field("name") String name,
-//            @Field("email") String email,
-//            @Field("password") String password,
-//            @Field("yearOfStudy") String year,
-//            @Field("alumni") String alumni
-//
-//    );
-
     @POST(REGISTER_USER)
-    Call<ResponseRegister> register(@Body HashMap<String, String> registerCredentials);
+    Call<ServerResponse> register(@Body HashMap<String, String> registerCredentials);
 
-    @FormUrlEncoded
-    @POST("login")
-    Call<ResponseLogin> login(
-            @Field("email") String email,
-            @Field("password") String password
+    @POST(LOGIN_USER)
+    Call<ServerResponse> login(@Body HashMap<String, String> loginCredentials);
 
-    );
+    @POST(PROFILE_USER)
+    Call<ServerResponse> profile(@Body HashMap<String, Integer> userid);
+
 }
