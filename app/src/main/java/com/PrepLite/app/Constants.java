2a7eafd9f6eee85df986fb59ebf1e7a634b038df
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2021-11-27 12:41:47 +0530

    Updated

--- app/src/main/java/com/PrepLite/app/Constants.java
+++ app/src/main/java/com/PrepLite/app/Constants.java
@@ -2,12 +2,11 @@ package com.PrepLite.app;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://82a7-157-48-248-163.ngrok.io/";
+    public static final String BASE_URL = "https://b612-112-133-232-80.ngrok.io/";
 
     public static final String REGISTER_USER = "register/";
     public static final String LOGIN_USER = "login/";
     public static final String PROFILE_USER = "profile/";
-
 
 
 }
