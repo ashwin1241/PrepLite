3b7e0d4228be3cd282306862deea7f2ed5e90a55
ashwin1241 <ashwinwadatkar@gmail.com>
2021-11-29 11:52:42 +0530

    commit before pull

--- app/src/main/java/com/PrepLite/ProfileEditActivity.java
+++ app/src/main/java/com/PrepLite/ProfileEditActivity.java
@@ -3,16 +3,18 @@ package com.PrepLite;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ProfileEditActivity extends AppCompatActivity {
 
-    private String[] Company_pair;
-    private String[] pair;
+    private String[] companies;
+    private ArrayList<Integer> compList;
     private boolean[] isSelectedCompany;
     private int company_id;
     private TextView SelectCompany;
@@ -24,10 +26,16 @@ public class ProfileEditActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(false);
         getSupportActionBar().setTitle("Edit Profile");
 
-        Company_pair = new String[5];
-        Company_pair[0]="Microsoft";
+        companies = new String[5];
+        companies[0]="Microsoft";
+        companies[1]="Amazon";
+        companies[2]="Oracle";
+        companies[3]="CodeNation";
+        companies[4]="Cisco";
 
-        isSelectedCompany = new boolean[Company_pair.length];
+        compList = new ArrayList<>();
+
+        isSelectedCompany = new boolean[companies.length];
         for(int i=0;i<isSelectedCompany.length;i++)
             isSelectedCompany[i]=false;
 
@@ -36,8 +44,50 @@ public class ProfileEditActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(ProfileEditActivity.this);
-                builder.setTitle("Select Company(s)");
-                //.setMultiChoiceItems()
+                builder.setTitle("Select Company(s)")
+                .setMultiChoiceItems(companies, isSelectedCompany, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                        if(b)
+                        {
+                            compList.add(i);
+                        }
+                        else
+                        {
+                            compList.remove(i);
+                        }
+                    }
+                })
+                .setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if(compList.size()>0)
+                        {
+                            String s=companies[compList.get(0)];
+                            for(int k=1;k<compList.size();k++)
+                            {
+                                s+=", "+companies[compList.get(k)];
+                            }
+                            SelectCompany.setText(s);
+                        }
+                        else
+                            SelectCompany.setText("Select Company");
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setNeutralButton("Clear all", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        isSelectedCompany = new boolean[companies.length];
+                        compList = new ArrayList<>();
+                        SelectCompany.setText("Select Company");
+                    }
+                });
                 builder.create().show();
             }
         });
