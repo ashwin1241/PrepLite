cef02319047260ba22ef40ac7a3a3e6f563b421c
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2021-12-02 21:17:36 +0530

    Updated

---
+++ app/src/main/java/com/PrepLite/activities/ChatWithUsersActivity.java
@@ -0,0 +1,19 @@
+package com.PrepLite.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.PrepLite.R;
+
+public class ChatWithUsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_with_users);
+
+
+
+    }
+}
